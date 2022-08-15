4aad2dc00e6fcb448be9fe18845f8a21082db9f6
sherazade othman <othmansherazade@gmail.com>
2022-08-15 14:17:42 +0200

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,23 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = answerquestion
+  end
+
+  private
+  def answerquestion
+    if @question == "I'm going to work"
+      return "Great!"
+
+    elsif
+     @question.end_with? "?"
+     return "Silly question, get dressed and go to work!"
+
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+end
